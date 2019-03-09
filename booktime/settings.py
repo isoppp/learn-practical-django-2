75da6610958b9968120ed1d80b3b64d5f8115db0
isoppp <prog@d2deck.com>
2019-03-09 20:19:41 +0900

    Fixed local db user

--- booktime/settings.py
+++ booktime/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "pratical-django-2",
-        "USER": "h.isogai",
+        "USER": "isoppp",
         "PASSWORD": "",
         "HOST": "127.0.0.1",
         "PORT": "5432",
