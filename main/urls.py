791c51c33235fe119342bbe9cdf9e0f724e8190e
isoppp <prog@d2deck.com>
2019-03-11 23:25:54 +0900

    Added detail view and connected from list view

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from django.views.generic.detail import DetailView
+from main import models
 
 from main import views
 
@@ -8,4 +10,5 @@ urlpatterns = [
     path("about-us/", TemplateView.as_view(template_name="about_us.html"), name="about_us"),
     path("contact-us/", views.ContactUsView.as_view(), name="contact_us"),
     path("products/<slug:tag>/", views.ProductListVIew.as_view(), name="products"),
+    path("product/<slug:slug>/", DetailView.as_view(model=models.Product), name="product"),
 ]
