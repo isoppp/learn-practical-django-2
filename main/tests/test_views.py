1eb8040308b0904ffeb7cfd0dbc25b3e5932a869
isoppp <prog@d2deck.com>
2019-03-11 23:18:01 +0900

    Added test for product list view

--- main/tests/test_views.py
+++ main/tests/test_views.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
 from django.urls import reverse
 
-from main import forms
+from decimal import Decimal
+
+from main import forms, models
 
 
 class TestPage(TestCase):
@@ -23,3 +25,35 @@ class TestPage(TestCase):
         self.assertTemplateUsed(response, "contact_form.html")
         self.assertContains(response, "BookTime")
         self.assertIsInstance(response.context["form"], forms.ContactForm)
+
+    def test_products_page_returns_active(self):
+        models.Product.objects.create(
+            name="The cathedral and the bazaar", slug="tale-two-cities", price=Decimal("2.00"), active=False
+        )
+
+        response = self.client.get(reverse("products", kwargs={"tag": "all"}))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "BookTime")
+
+        product_list = models.Product.objects.active().order_by("name")
+
+        self.assertEqual(list(response.context["object_list"]), list(product_list))
+
+    def test_products_page_filters_by_tags_and_active(self):
+        cb = models.Product.objects.create(
+            name="The cathedral and the bazaar", slug="cathedral-bazzar", price=Decimal("10.00")
+        )
+        cb.tags.create(name="Open source", slug="opensource")
+        models.Product.objects.create(
+            name="Microsoft Windows guid", slug="microsoft-windows-guid", price=Decimal("12.00")
+        )
+
+        response = self.client.get(reverse("products", kwargs={"tag": "opensource"}))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "BookTime")
+
+        product_list = models.Product.objects.active().filter(tags__slug="opensource").order_by("name")
+
+        self.assertEqual(list(response.context["object_list"]), list(product_list))
