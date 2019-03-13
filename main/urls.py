79476be228789947c7bb0c1a1df3a4837b12f5f9
isoppp <prog@d2deck.com>
2019-03-13 12:42:58 +0900

    Added views for Address model

--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,10 @@ from main import models
 from main import views
 
 urlpatterns = [
+    path("address/", views.AddressListView.as_view(), name="address_list"),
+    path("address/create/", views.AddressCreateView.as_view(), name="address_create"),
+    path("address/<int:pk>/", views.AddressUpdateView.as_view(), name="address_update"),
+    path("address/<int:pk>/delete/", views.AddressDeleteView.as_view(), name="address_delete"),
     path("signup/", views.SignupView.as_view(), name="signup"),
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
     path("about-us/", TemplateView.as_view(template_name="about_us.html"), name="about_us"),
