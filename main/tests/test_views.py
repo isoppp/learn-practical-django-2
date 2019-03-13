cd94946db447c30eabaf40cb8be06db3a29e6488
isoppp <prog@d2deck.com>
2019-03-13 14:12:34 +0900

    Added test for address views

--- main/tests/test_views.py
+++ main/tests/test_views.py
@@ -10,6 +10,9 @@ from main import forms, models
 
 
 class TestPage(TestCase):
+
+    # Test for pages
+
     def test_home_page_works(self):
         response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
@@ -28,6 +31,8 @@ class TestPage(TestCase):
         self.assertTemplateUsed(response, "contact_form.html")
         self.assertContains(response, "BookTime")
         self.assertIsInstance(response.context["form"], forms.ContactForm)
+
+    # Test for products
 
     def test_products_page_returns_active(self):
         models.Product.objects.create(
@@ -61,6 +66,8 @@ class TestPage(TestCase):
 
         self.assertEqual(list(response.context["object_list"]), list(product_list))
 
+    # Test for signup
+
     def test_user_signup_page_loads_correctly(self):
         response = self.client.get(reverse("signup"))
         self.assertEqual(response.status_code, 200)
@@ -78,3 +85,39 @@ class TestPage(TestCase):
         self.assertTrue(models.User.objects.filter(email="user@domain.com").exists())
         self.assertTrue(auth.get_user(self.client).is_authenticated)
         mock_send.assert_called_once()
+
+    # Test for address
+
+    def test_address_list_page_returns_only_owned(self):
+        user1 = models.User.objects.create_user("user1", "pw432joij")
+        user2 = models.User.objects.create_user("user2", "pw432joij")
+        models.Address.objects.create(
+            user=user1, name="john kimball", address1="flat 2", address2="12 Stralz avenue", city="London", country="uk"
+        )
+        models.Address.objects.create(
+            user=user2, name="marc kimball", address1="123 Deacon road", city="London", country="uk"
+        )
+
+        self.client.force_login(user2)
+        response = self.client.get(reverse("address_list"))
+
+        self.assertEqual(response.status_code, 200)
+
+        address_list = models.Address.objects.filter(user=user2)
+        self.assertEqual(list(response.context["object_list"]), list(address_list))
+
+    def test_address_create_stores_user(self):
+        user1 = models.User.objects.create_user("user1", "pw432joij")
+        post_data = {
+            "name": "john kercher",
+            "address1": "1 av st",
+            "address2": "",
+            "zip_code": "MA12GS",
+            "city": "Manchester",
+            "country": "uk",
+        }
+
+        self.client.force_login(user1)
+        self.client.post(reverse("address_create"), post_data)
+
+        self.assertTrue(models.Address.objects.filter(user=user1).exists())
