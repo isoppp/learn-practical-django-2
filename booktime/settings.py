d6e63c4c9112b0bea3ae39e0d3178adcba67d6ab
isoppp <prog@d2deck.com>
2019-03-16 23:50:14 +0900

    fix db user for dev

--- booktime/settings.py
+++ booktime/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "pratical-django-2",
-        "USER": "h.isogai",
+        "USER": "isoppp",
         "PASSWORD": "",
         "HOST": "127.0.0.1",
         "PORT": "5432",
