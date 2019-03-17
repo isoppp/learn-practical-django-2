18ea7d7a7855fa38a70a71a79e99933be1680588
isoppp <prog@d2deck.com>
2019-03-17 22:35:09 +0900

    Added login form

--- main/forms.py
+++ main/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth import authenticate
 from django.core.mail import send_mail
 from django.contrib.auth.forms import UserCreationForm as DjangoUserCreationForm
 from django.contrib.auth.forms import UsernameField
@@ -37,6 +38,33 @@ class UserCreationForm(DjangoUserCreationForm):
         send_mail(
             "Welcome to BookTime", message, "site@booktime.domain", [self.cleaned_data["email"]], fail_silently=True
         )
+
+
+class AuthenticationForm(forms.Form):
+    email = forms.EmailField()
+    password = forms.CharField(strip=False, widget=forms.PasswordInput)
+
+    def __init__(self, request=None, *args, **kwargs):
+        self.request = request
+        self.user = None
+        super().__init__(*args, **kwargs)
+
+    def clean(self):
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+
+        if email is not None and password:
+            self.user = authenticate(self.request, email=email, password=password)
+
+            if self.user is None:
+                raise forms.ValidationError("Invalid email/password combination.")
+
+            logger.info("Authentication successful for email=%s", email)
+
+        return self.cleaned_data
+
+    def get_user(self):
+        return self.user
 
 
 BasketLineFormSet = inlineformset_factory(models.Basket, models.BasketLine, fields=("quantity",), extra=0)
--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.views.generic import TemplateView
 from django.views.generic.detail import DetailView
-from main import models
+from django.contrib.auth import views as auth_views
 
+from main import models, forms
 from main import views
 
 urlpatterns = [
@@ -11,6 +12,11 @@ urlpatterns = [
     path("address/<int:pk>/", views.AddressUpdateView.as_view(), name="address_update"),
     path("address/<int:pk>/delete/", views.AddressDeleteView.as_view(), name="address_delete"),
     path("signup/", views.SignupView.as_view(), name="signup"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="login.html", form_class=forms.AuthenticationForm),
+        name="login",
+    ),
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
     path("about-us/", TemplateView.as_view(template_name="about_us.html"), name="about_us"),
     path("contact-us/", views.ContactUsView.as_view(), name="contact_us"),
