547a10c4b6a2584e4dbf138792e4780e677a1dd2
isoppp <prog@d2deck.com>
2019-04-07 22:17:44 +0900

    Added signal that change order status when order line saved

--- main/signals.py
+++ main/signals.py
@@ -5,11 +5,11 @@ import logging
 from PIL import Image
 
 from django.core.files.base import ContentFile
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 from django.contrib.auth.signals import user_logged_in
 
-from .models import ProductImage, Basket
+from .models import ProductImage, Basket, OrderLine, Order
 
 THUMBNAIL_SIZE = (300, 300)
 
@@ -51,3 +51,11 @@ def merge_baskets_if_found(sender, user, request, **kwargs):
             anonymous_basket.user = user
             anonymous_basket.save()
             logger.info("Assigned user to basket id %d")
+
+
+@receiver(post_save, sender=OrderLine)
+def orderline_to_order_status(sender, instance, **kwargs):
+    if not instance.order.lines.filter(status__lt=OrderLine.SENT).exists():
+        logger.info("All lines for order %d have been processed. Marking as done.", instance.order.id)
+        instance.order.status = Order.DONE
+        instance.order.save()
