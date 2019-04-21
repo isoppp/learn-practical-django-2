8d64b2cae3efadd0407ad5acba694b934ed11e11
isoppp <prog@d2deck.com>
2019-04-21 23:48:35 +0900

    Added active chat counter with eventsource, but hasn't worked yet

--- booktime/routing.py
+++ booktime/routing.py
@@ -1,10 +1,12 @@
+from django.urls import re_path
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.http import AsgiHandler
 import main.routing
 
 application = ProtocolTypeRouter(
     {
-        # (http->django views is added by default)
-        "websocket": AuthMiddlewareStack(URLRouter(main.routing.websocket_urlpatterns))
+        "websocket": AuthMiddlewareStack(URLRouter(main.routing.websocket_urlpatterns)),
+        "http": URLRouter(main.routing.http_urlpatterns + [re_path(r"", AsgiHandler)]),
     }
 )
--- main/routing.py
+++ main/routing.py
@@ -1,4 +1,6 @@
+from channels.auth import AuthMiddlewareStack
 from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [path("ws/customer-service/<int:order_id>/", consumers.ChatConsumer)]
+http_urlpatterns = [path("customer-service/notify/", AuthMiddlewareStack(consumers.ChatNotifyConsumer))]
