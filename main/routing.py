395b58350da09cbda2c9fa5f05499da079d4fa25
isoppp <prog@d2deck.com>
2019-05-01 22:38:17 +0900

    Added OrderTrackerCunsumer route

--- main/routing.py
+++ main/routing.py
@@ -3,4 +3,7 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [path("ws/customer-service/<int:order_id>/", consumers.ChatConsumer)]
-http_urlpatterns = [path("customer-service/notify/", AuthMiddlewareStack(consumers.ChatNotifyConsumer))]
+http_urlpatterns = [
+    path("customer-service/notify/", AuthMiddlewareStack(consumers.ChatNotifyConsumer)),
+    path("mobile-api/my-orders/<int:order_id>/tracker/", consumers.OrderTrackerConsumer),
+]
