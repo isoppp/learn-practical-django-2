e4e1a2ff5401696d20b79930c20095f3b5bca019
isoppp <prog@d2deck.com>
2019-05-04 23:24:49 +0900

    Apply custom auth routing

--- booktime/routing.py
+++ booktime/routing.py
@@ -1,12 +1,12 @@
 from django.urls import re_path
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.http import AsgiHandler
 import main.routing
+from .auth import TokenGetAuthMiddlewareStack
 
 application = ProtocolTypeRouter(
     {
-        "websocket": AuthMiddlewareStack(URLRouter(main.routing.websocket_urlpatterns)),
+        "websocket": TokenGetAuthMiddlewareStack(URLRouter(main.routing.websocket_urlpatterns)),
         "http": URLRouter(main.routing.http_urlpatterns + [re_path(r"", AsgiHandler)]),
     }
 )
