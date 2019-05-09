6acaf6ae5ae9ce953cafdf2c688277e4049e79f5
isoppp <prog@d2deck.com>
2019-05-09 17:40:14 +0900

    Rewrite App.js

--- booktime_mobile/App.js
+++ booktime_mobile/App.js
@@ -1,21 +1,28 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import LoginView from './src/components/LoginView'
+import ChatView from './src/components/ChatView'
+import OrderView from './src/components/OrderView'
+import BackendApi from './src/backend'
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+  constructor (props) {
+    super(props)
+    this.backendApi = new BackendApi()
+    this.state = {
+      loggedIn: false
+    }
+  }
+
+  render () {
+    if (this.state.loggedIn) {
+      return <OrderView backendApi={this.backendApi}/>
+    } else {
+      return (
+        <LoginView
+          backendApi={this.backendApi}
+          setLoggedIn={() => this.setState({ loggedIn: true })}
+        />
+      )
+    }
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
